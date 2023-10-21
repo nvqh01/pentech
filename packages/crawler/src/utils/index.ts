d2f99d4a49cb56124dc241e7465d1bb81b7169bb
huynvq <nvqh01@gmail.com>
2023-10-21 20:03:26 +0700

    refactor: improve utils of package crawler

--- packages/crawler/src/utils/index.ts
+++ packages/crawler/src/utils/index.ts
@@ -1,9 +1,20 @@
-import { Request, RequestOptions } from 'crawlee';
+import { getUuid } from '@pentech/core';
+import { Request, RequestOptions as BaseRequestOptions } from 'crawlee';
+
+interface RequestOptions extends BaseRequestOptions {
+  autoCreateUniqueKey?: boolean;
+}
 
 export function createRequests(
   options: RequestOptions | RequestOptions[],
 ): Request[] {
   !Array.isArray(options) && (options = [options]);
+
+  options = options.map(({ autoCreateUniqueKey, ..._options }) => {
+    autoCreateUniqueKey && (_options.uniqueKey = getUuid());
+    return _options;
+  });
+
   return options.map<Request>(_options => new Request({ ..._options }));
 }
 
